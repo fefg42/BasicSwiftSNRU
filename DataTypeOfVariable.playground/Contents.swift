f11a162eaa4cade3ad777cd8b1f13dfdbab3ffe7
breeze <breeze@abc.com>
2019-03-18 14:28:12 +0700

    Start DataType

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon"//นี่คือก่รประกาศที่อาศัยชนิดของ Value บอก Datatype
+var mySurname:String = "AAAAAA"//นี้
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicit Variable
+//การประกาศตัวแปรน่ะจ้ะ
+
+var myName = "breeze"
+myName = "Doramon"
+
+//Explicit Constant
+var myFriend = "Nopita"
+//myFriend = "Sunako"Error เพราะfriend เป็นค่าคงที่
+
